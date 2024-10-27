8e7ba6b29815bf1f70b263c0094c5b0c36e3b8c4
Aleksahdr Daniliants <isken2046@yandex.ru>
2024-10-27 17:20:28 +0300

    Add ApiRequestsConstants.java

--- src/main/java/praktikum/constants/ApiRequestsConstants.java
+++ src/main/java/praktikum/constants/ApiRequestsConstants.java
@@ -1,2 +1,5 @@
-package praktikum.constants;public class ApiRequestsConstants {
+package praktikum.constants;
+
+public class ApiRequestsConstants {
+    public static final String API_REQUEST_URI = "https://stellarburgers.nomoreparties.site/";
 }
