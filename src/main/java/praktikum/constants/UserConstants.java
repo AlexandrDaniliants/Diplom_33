f40a0a9ae091d388f9d97e0df9b6be10d86d7276
Aleksahdr Daniliants <isken2046@yandex.ru>
2024-10-27 17:22:11 +0300

    Add UserConstants.java

--- src/main/java/praktikum/constants/UserConstants.java
+++ src/main/java/praktikum/constants/UserConstants.java
@@ -1,2 +1,11 @@
-package praktikum.constants;public class UserConstants {
+package praktikum.constants;
+
+public class UserConstants {
+    public static final String USER_NAME = "User";
+    public static final String USER_EMAIL = "@yandex.ru";
+    public static final String USER_PASSWORD = "pass";
+
+    public static final String CREATE_USER_PATH = "/api/auth/register";
+    public static final String LOGIN_USER_PATH = "/api/auth/login";
+    public static final String DELETE_USER_PATH = "/api/auth/user";
 }
