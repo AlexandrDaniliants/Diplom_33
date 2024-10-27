fb9a71d1074ebe62cf1aaf812fc695c497551a12
Aleksahdr Daniliants <isken2046@yandex.ru>
2024-10-27 17:21:26 +0300

    Add MessageConstants.java

--- src/main/java/praktikum/constants/MessageConstants.java
+++ src/main/java/praktikum/constants/MessageConstants.java
@@ -1,2 +1,14 @@
-package praktikum.constants;public class MessageConstants {
+package praktikum.constants;
+
+public class MessageConstants {
+
+    public static final String WARNING_INCORRECT_PASSWORD_NOT_APPEAR = "Предупреждение о некорректном пароле не появилось";
+    public static final String SET_ORDER_BUTTON_NOT_APPEAR = "Кнопка Оформить заказ не появилась";
+    public static final String AUTHORIZATION_PAGE_NOT_LOAD = "Страница авторизации не загрузилась";
+    public static final String EXIT_BUTTON_FAILED = "Выход по кнопке Выйти не удался";
+    public static final String LOGIN_TO_PERSONAL_ACCOUNT_NOT_COMPLETED = "Вход в личный кабинет не выполнен";
+    public static final String TRANSITION_TO_CONSTRUCTOR_FAILED = "Переход в конструктор не удался";
+    public static final String BUN_SECTION_NOT_ACTIVE = "Секция Булки не активна";
+    public static final String FILLING_SECTION_NOT_ACTIVE = "Секция Начинки не активна";
+    public static final String SAUCE_SECTION_NOT_ACTIVE = "Секция Соусы не активна";
 }
