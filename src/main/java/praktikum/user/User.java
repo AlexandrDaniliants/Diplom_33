95e3645679def7e1db23e2838e6805299658487b
Aleksahdr Daniliants <isken2046@yandex.ru>
2024-10-27 17:30:26 +0300

    Add User.java

--- src/main/java/praktikum/user/User.java
+++ src/main/java/praktikum/user/User.java
@@ -1,2 +1,42 @@
-package praktikum.user;public class User {
+package praktikum.user;
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+
+    public User(String userEmail, String userPassword) {
+        this.email = userEmail;
+        this.password = userPassword;
+    }
+
+    public User(String userName, String userEmail, String userPassword) {
+        this.name = userName;
+        this.email = userEmail;
+        this.password = userPassword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
