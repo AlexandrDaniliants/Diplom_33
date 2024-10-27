a8a215bc461ee2926ff7bdc9d03a99d924eb7a08
Aleksahdr Daniliants <isken2046@yandex.ru>
2024-10-27 17:30:51 +0300

    Add UserRandomData.java

--- src/main/java/praktikum/user/UserRandomData.java
+++ src/main/java/praktikum/user/UserRandomData.java
@@ -1,2 +1,32 @@
-package praktikum.user;public class UserRandomData {
+package praktikum.user;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import static praktikum.constants.UserConstants.*;
+
+public class UserRandomData {
+
+    public static User generateRandomUser() {
+        return new User(userRandomName(), userRandomEmail(), userRandomPassword());
+    }
+
+    public static User generateRandomUserWithIncorrectPassword() {
+        return new User(userRandomName(), userRandomEmail(), userIncorrectRandomPassword());
+    }
+
+    public static String userRandomName(){
+        return (USER_NAME + RandomStringUtils.randomAlphanumeric(5, 10));
+    }
+
+    public static String userRandomEmail(){
+        return (RandomStringUtils.randomAlphanumeric(3, 10) + USER_EMAIL);
+    }
+
+    public static String userRandomPassword(){
+        return (USER_PASSWORD + RandomStringUtils.randomAlphanumeric(2, 5));
+    }
+
+    public static String userIncorrectRandomPassword(){
+        return (RandomStringUtils.randomAlphanumeric(2, 5));
+    }
 }
