ecc7b53ffe0b8bf4e4f1be44a7699ccc1b595c62
Aleksahdr Daniliants <isken2046@yandex.ru>
2024-10-27 17:29:53 +0300

    Add UserRequest.java

--- src/main/java/praktikum/requests/UserRequest.java
+++ src/main/java/praktikum/requests/UserRequest.java
@@ -1,2 +1,45 @@
-package praktikum.requests;public class UserRequest {
+package praktikum.requests;
+
+import io.qameta.allure.Step;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import praktikum.user.User;
+
+import static io.restassured.RestAssured.given;
+import static praktikum.constants.UserConstants.*;
+
+public class UserRequest extends ApiUri{
+
+    private User user;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Step("Создание пользователя")
+    public void createUser(User user){
+        setUpUri();
+        given()
+                .contentType(ContentType.JSON)
+                .body(user)
+                .post(CREATE_USER_PATH);
+    }
+
+    @Step("Авторизация пользователя")
+    public Response loginUser(User user) {
+        setUpUri();
+        return given()
+                .contentType(ContentType.JSON)
+                .body(user)
+                .post(LOGIN_USER_PATH);
+    }
+
+    @Step("Удаление пользователя")
+    public void deleteUser(String accessToken) {
+        setUpUri();
+        given().log().all()
+                .header("Authorization", accessToken)
+                .when()
+                .delete(DELETE_USER_PATH);
+    }
 }
