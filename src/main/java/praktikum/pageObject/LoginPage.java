f640e8547f99ef98659a09f39217a83d2986e872
Aleksahdr Daniliants <isken2046@yandex.ru>
2024-10-27 17:22:51 +0300

    Add LoginPage.java

--- src/main/java/praktikum/pageObject/LoginPage.java
+++ src/main/java/praktikum/pageObject/LoginPage.java
@@ -1,2 +1,44 @@
-package praktikum.pageObject;public class LoginPage {
+package praktikum.pageObject;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+
+import static praktikum.constants.LocatorConstants.*;
+
+public class LoginPage extends VisibilityWaiter {
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Step("Ожидание загрузки страницы авторизации")
+    public void waitForLoadAuthorizationPage() {
+        waitForVisibility(HEADLINE_LOGIN);
+    }
+
+    @Step("Клик по ссылке Зарегистрироваться")
+    public void clickRegisterLink() {
+        driver.findElement(REGISTER_LINK).click();
+    }
+
+    @Step("Заполнение формы авторизации")
+    public void completingAuthorizationForm(String email, String password) {
+        driver.findElement(EMAIL_LOGIN_FIELD).sendKeys(email);
+        driver.findElement(PASSWORD_LOGIN_FIELD).sendKeys(password);
+    }
+
+    @Step("Клик по кнопке Войти")
+    public void clickLoginButton() {
+        driver.findElement(LOGIN_BUTTON).click();
+    }
+
+    @Step("Клик по ссылке Восстановить пароль")
+    public void clickForgotPasswordLink() {
+        driver.findElement(FORGOT_PASSWORD_LINK).click();
+    }
+
+    @Step("Проверка видимости заголовка Войти")
+    public boolean checkVisibilityHeadlineLogin() {
+        return driver.findElement(HEADLINE_LOGIN).isDisplayed();
+    }
 }
