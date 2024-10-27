1758820dbdc5d25e6d01592bbc79acd89142452a
Aleksahdr Daniliants <isken2046@yandex.ru>
2024-10-27 17:29:20 +0300

    Add ApiUri.java

--- src/main/java/praktikum/requests/ApiUri.java
+++ src/main/java/praktikum/requests/ApiUri.java
@@ -1,2 +1,11 @@
-package praktikum.requests;public class ApiUri {
+package praktikum.requests;
+
+import io.restassured.RestAssured;
+
+import static praktikum.constants.ApiRequestsConstants.API_REQUEST_URI;
+
+public class ApiUri {
+    public static void setUpUri() {
+        RestAssured.baseURI = API_REQUEST_URI;
+    }
 }
